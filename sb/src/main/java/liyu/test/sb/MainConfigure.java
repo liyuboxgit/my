285bc8dadad7cfd9e8ebee50d8c734917f638e7f
liyu <309147857@qq.com>
2018-02-04 09:27:51 +0800

    sb html

--- sb/src/main/java/liyu/test/sb/MainConfigure.java
+++ sb/src/main/java/liyu/test/sb/MainConfigure.java
@@ -18,6 +18,7 @@ public class MainConfigure extends WebMvcConfigurerAdapter{
 	@Override
    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/mh/**").addResourceLocations("classpath:/liyu/test/sb/mh/");
     }
 	
 	@Bean
