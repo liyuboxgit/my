67abb3ab843fa98b4e49679af957d10ed0944c61
liyu <liyu@rthdtax.com>
2018-09-20 09:45:29 +0800

    del duboo

--- dubbo/dubbo-consumer/src/main/java/org/dubbo/consumer/service/ConsumerService.java
+++
@@ -1,5 +0,0 @@
-package org.dubbo.consumer.service;
-
-public interface ConsumerService {
-	public String ConService() throws Exception;
-}
--- dubbo/dubbo-provider/src/main/java/org/dubbo/provider/service/DemoServices.java
+++
@@ -1,5 +0,0 @@
-package org.dubbo.provider.service;
-
-public interface DemoServices {
-    public String toProvider(String name) throws Exception;
-}
